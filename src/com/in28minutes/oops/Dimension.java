5f04357401073bc1a8fc3512e9f63df691414480
kareem <kshaik_sr2021@yahoo.com>
2024-07-28 14:27:04 +0100

    Section 10 Exercise: Dimensions completed

---
+++ src/com/in28minutes/oops/Dimension.java
@@ -0,0 +1,58 @@
+package com.in28minutes.oops;
+
+public class Dimension {
+    public void setFeet(int feet) {
+        this.feet = feet;
+    }
+
+    public void setInches(int inches) {
+        this.inches = inches;
+    }
+
+    private int feet;
+    private int inches;
+
+    public Dimension(int inches) {
+        //25/12 = 2
+        //25 % 12 = 1
+
+        //25 inches
+        //12 inches = 1 foot
+        //25 inches = 2 foot 1 inches
+
+        // TODO: Convert the total inches into feet and inches. One foot is 12 inches.
+        // Store the feet and inches in their respective instance variables.
+
+        //Dimension(int inches) - This constructor will take one parameter, inches (an integer), and it should convert the given inches into feet and inches.
+        //If the input inches are less than 0, set feet and inches to -1 to indicate invalid input. One foot is equal to 12 inches.
+
+        //25 inches
+        //12 inches = 1 foot
+        //25 inches = 2 foot 1 inches
+
+        if(inches < 0) {
+            this.feet = -1;
+            this.inches = -1;
+        }
+//        else if (inches == 0 ) {
+//            feet = 0;
+//            this.inches = 0;
+//        }
+        else {
+            this.feet = inches / 12;
+            this.inches = inches % 12;
+        }
+    }
+
+    public int getFeet() {
+        // TODO: Return the value of feet.
+//        getFeet() - This method should return the calculated feet from the constructor.
+        return feet;
+    }
+
+    public int getInches() {
+        // TODO: Return the value of inches.
+//        getInches() - This method should return the remaining inches (less than 12) from the constructor.
+        return inches;
+    }
+}
---
+++ src/com/in28minutes/oops/DimensionRunner.java
@@ -0,0 +1,22 @@
+package com.in28minutes.oops;
+
+public class DimensionRunner {
+
+    public static void main(String[] args) {
+
+        Dimension dim = new Dimension(25);
+        System.out.println("Given Inches: 25 is equivalent to: ");
+        System.out.print(dim.getFeet() + " Feet and ");
+        System.out.print(dim.getInches() + " Inches");
+
+        Dimension dim1 = new Dimension(-1);
+        System.out.println("\n\nGiven Inches: -1 is equivalent to: ");
+        System.out.print(dim1.getFeet() + " Feet and ");
+        System.out.print(dim1.getInches() + " Inches");
+
+        Dimension dim0 = new Dimension(0);
+        System.out.println("\n\nGiven Inches: 0 is equivalent to: ");
+        System.out.print(dim0.getFeet() + " Feet and ");
+        System.out.print(dim0.getInches() + " Inches");
+    }
+}
