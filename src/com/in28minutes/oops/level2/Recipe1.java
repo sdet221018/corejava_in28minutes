9c8cc9d7c4def1cca4c8e4e2883fb750fbf36dca
kareem <kshaik_sr2021@yahoo.com>
2024-07-30 17:37:06 +0100

    244. Step 14 - Creating Recipes with Template M

---
+++ src/com/in28minutes/oops/level2/Recipe1.java
@@ -0,0 +1,21 @@
+package com.in28minutes.oops.level2;
+
+public class Recipe1 extends AbstractRecipe{
+
+
+    @Override
+    void getReady() {
+        System.out.println("Get the Raw Materials");
+        System.out.println("Get the Utensils");
+    }
+
+    @Override
+    void doTheDish() {
+        System.out.println("Do the Dish");
+    }
+
+    @Override
+    void cleanUp() {
+        System.out.println("Cleanup the Utensils");
+    }
+}
